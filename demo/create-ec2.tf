a794deb864070642739d28faa19e162980287569
US\varunb3 <varunb3@deloitte.com>
2021-08-23 16:29:36 +0530

    commit - us-west-2

--- demo/create-ec2.tf
+++ demo/create-ec2.tf
@@ -1,22 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-  required_version = ">=0.14.9"
+  
+  required_version = ">= 0.14.9"
 }
+
 provider "aws" {
   profile = "default"
-  region  = "us-east-2"
-
+  region = "us-west-2"
 }
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
 
+resource "aws_instance" "app_server" {
+  ami = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+  
   tags = {
-    Name = "Varun from terraform"
+    Name = "Varun from Terraform"
   }
 }
