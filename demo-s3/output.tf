c0b3e1a3a82160c13e560b741673f7ce6e9bc660
US\varunb3 <varunb3@deloitte.com>
2021-08-24 10:38:34 +0530

    test

--- demo-s3/output.tf
+++ demo-s3/output.tf
@@ -2,3 +2,13 @@ output "arn" {
   description = "Name of the S3 bucket"
   value       = aws_s3_bucket.s3.arn
 }
+
+output "bucket" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.s3.bucket
+}
+
+output "tags_all" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.s3.tags_all.Name
+}
