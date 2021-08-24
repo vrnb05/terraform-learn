52e5a147c86e5e1e8b4a8211d80cfef4bc9c63db
varun <vrnb05@gmail.com>
2021-08-24 15:38:41 +0530

    Demo 4 added

---
+++ demo4/main.tf
@@ -0,0 +1,33 @@
+resource "aws_key_pair" "key" {
+  key_name = "key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
+resource "aws_instance" "example" {
+    ami  = var.AMIS[var.AWS_REGION]
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.key.key_name
+    
+    provisioner "file" {
+        source = "script.sh"
+        destination = "/tmp/script.sh"    
+    }
+provisioner "remote-exec" {
+  inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh",
+      "sudo /tmp/script.sh",
+  ]
+}
+
+  tags = {
+    Name = var.instance-name
+  }
+
+  connection {
+    host = coalesce(self.public_ip, self.private_ip)
+    type = "ssh"
+    user = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+}
